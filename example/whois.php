ce2ea6f17a634d0a75764fdd9e490407004074d5
James Wade <jpswade@gmail.com>
2019-05-23 07:38:09 +0100

    fix Update example to allows args

--- example/whois.php
+++ example/whois.php
@@ -3,4 +3,6 @@
 include __DIR__ . '/../src/Whois/Lookup.php';
 
 $lookup = new Deaduseful\Whois\Lookup();
-echo $lookup->lookup('example.com', 'whois.verisign-grs.com');
+$query = isset($argv[1]) ? $argv[1] : 'example.com';
+$host = 'whois.verisign-grs.com';
+echo $lookup->lookup($query, $host);
