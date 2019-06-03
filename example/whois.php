a73cbf9776686139aca2de11ae8a79f158ccf07f
James Wade <jpswade@gmail.com>
2019-06-03 22:34:36 +0100

    fix: add an example

--- example/whois.php
+++ example/whois.php
@@ -4,5 +4,6 @@ include __DIR__ . '/../src/Whois/Lookup.php';
 
 $lookup = new Deaduseful\Whois\Lookup();
 $query = isset($argv[1]) ? $argv[1] : 'example.com';
-$host = 'whois.verisign-grs.com';
-echo $lookup->lookup($query, $host);
+$host = isset($argv[2]) ? $argv[2] : 'whois.verisign-grs.com';
+$port = isset($argv[3]) ? $argv[3] : 43;
+echo $lookup->lookup($query, $host, $port);
