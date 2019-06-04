d527e68af3d095b355ee47f2f94906134242e54c
James Wade <jpswade@gmail.com>
2019-06-04 20:26:05 +0100

    fix: add test

---
+++ tests/LookupTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Deaduseful\Whois\Lookup;
+use PHPUnit\Framework\TestCase;
+
+class lookupTest extends TestCase
+{
+    public function testLookup()
+    {
+        $query = 'example.com';
+        $lookup = new Lookup();
+        $result = $lookup->lookup($query);
+        $this->assertStringContainsStringIgnoringCase($query, $result);
+    }
+}
